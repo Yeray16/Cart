aa8a778c30d70e5e9566bb7a48e263ae9afc5373
Yeray Navarro <yeraynp16@gmail.com>
2024-11-20 19:51:07 +0000

    Hecho hasta las 20:00

--- connection.php
+++ connection.php
@@ -2,6 +2,7 @@
 $user = 'root';
 $password = '';
 $dsn = 'mysql:host=localhost;dbname=tienda';
+<<<<<<< HEAD
 
 try {
   $link = new PDO($dsn, $user, $password);
@@ -9,3 +10,11 @@ try {
 } catch (PDOException $ex) {
   die ("Error en la conexión: ". $ex->getMessage());
 }
+=======
+try {
+  $link = new PDO($dsn, $user, $password);
+  $link->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (Exception $e) {
+  die ('Error en la conexión: ' . $e->getMessage());
+}
+>>>>>>> 2eb0213 (Hecho hasta las 20:00)
---
+++ home.php
@@ -0,0 +1,64 @@
+<?php
+  session_start();
+  require 'connection.php';
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Home Page</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+  <h1>Tabla de productos</h1>
+  <h2>Productos dentro del carrito: </h2>
+  <?php
+    try {
+      
+      if (isset($_GET['order'])) {
+        setcookie('order', $_GET['order'], time() + 3600, "/");
+        header("Location: home.php");
+      }
+
+      $order = isset($_COOKIE['order']) ? $_COOKIE['order'] : 'name';   
+      $stmtProducts = $link->prepare("SELECT id, name, price, amount FROM products ORDER BY $order");
+      $stmtProducts->execute();
+      $products = $stmtProducts->fetchAll(PDO::FETCH_OBJ);
+
+      if(isset($_GET['id'])){
+        $productId = $_GET['id'];
+        if(isset($_SESSION['cart'][$productId])){
+          $_SESSION['cart'][$productId]['quantity']++;
+        } else{
+          $stmtAddCart = $link->prepare('SELECT id, name, price FROM products WHERE id=:id');
+          $stmtAddCart->bindParam(':id', $produc)
+        }
+
+      }
+  ?>
+  <table>
+    <thead>
+      <tr>
+        <th><a href="home.php?order=name">Nombre</a></th>
+        <th><a href="home.php?order=price">Precio</a></th>
+        <th><a href="home.php?order=amount">Cantidad</a></th>
+        <th></th>
+      </tr>
+    </thead>
+    <tbody>
+      <?php
+        foreach($products as $product) {
+          printf('<tr><td>%s</td><td>%.2f</td><td>%d</td><td><a href=home.php?id=%d><button>Añadir al carrito</button></a></td>',
+                  $product->name, $product->price, $product->amount, $product->id);
+        }
+      ?>
+    </tbody>
+  </table>
+  <?php
+    } catch(Exception $e) {
+      die('Error: '.$e->getMessage());
+    } 
+  ?>
+</body>
+</html>
